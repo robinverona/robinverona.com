e5b53cfc7b977bb985c06bc64a4a38456a098c38
Robin Verona <contact@robinverona.com>
2023-04-10 12:42:35 +0200

    update js with menu toggle

--- js/app.js
+++ js/app.js
@@ -20,32 +20,32 @@
 // })
 
 
-// const mobileMenu = document.querySelector('.sidebar');
-// const mobileMenuButton = document.querySelector('#sidebar__button--open');
-// const mobileCloseMenuButton = document.querySelector('#sidebar-header__button--close');
-// console.log(mobileMenu);
+const mobileMenu = document.querySelector('.sidebar');
+const mobileMenuButton = document.querySelector('#sidebar__button--open');
+const mobileCloseMenuButton = document.querySelector('#sidebar-header__button--close');
+console.log(mobileMenu);
 
-// addEventListener('load', () => {
-//   console.log(mobileMenu.classList);
-//   console.log(window.innerHeight);
-//   mobileMenu.style.height = `${window.innerHeight}px`;
-// });
+addEventListener('load', () => {
+  console.log(mobileMenu.classList);
+  console.log(window.innerHeight);
+  mobileMenu.style.height = `${window.innerHeight}px`;
+});
 
-// mobileMenuButton.addEventListener('click', () => {
-//     mobileMenu.classList.remove('sidebar--hidden');
-//     mobileMenu.classList.add('sidebar--open');
-//     document.querySelector('body').style.position = 'fixed';
-//     console.log(mobileMenu.classList);
-//     console.log(window.innerHeight);
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.remove('sidebar--hidden');
+    mobileMenu.classList.add('sidebar--open');
+    document.querySelector('body').style.position = 'fixed';
+    console.log(mobileMenu.classList);
+    console.log(window.innerHeight);
 
-// });
+});
 
-//   mobileCloseMenuButton.addEventListener('click', () => {
-//     mobileMenu.classList.remove('sidebar--open');
-//     mobileMenu.classList.add('sidebar--hidden');
-//     document.querySelector('body').style.position = 'static';
-//     console.log(mobileMenu.classList);
-//   })
+  mobileCloseMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.remove('sidebar--open');
+    mobileMenu.classList.add('sidebar--hidden');
+    document.querySelector('body').style.position = 'static';
+    console.log(mobileMenu.classList);
+  })
 
 
 
